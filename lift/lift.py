fe3a446e279c864ac73ac3e117adfc505b5e1499
Ilja Kazantsev <33126420+4568520@users.noreply.github.com>
2018-02-20 15:51:50 +0200

    25

--- lift/lift.py
+++ lift/lift.py
@@ -18,8 +18,7 @@ theCabin = Cabine()#Object CROMIUM DEATH IS CREATED
 
 N=78 #Floors OF DEATH HOROR AND CULTS OF OLD ONE'S
 CabinButtons = []
-upButtons = []
-downButtons = []
+stageButtons = []
 i=0
 while i<=N:
     newButton=CabineButton()
@@ -33,12 +32,32 @@ while i<=N:
     if i!=N:
         newButton.stage = i
         newButton.direction = 1
-        upButtons.append(newButton)
+        stageButtons.append(newButton)
     if i>1:
         newButton.stage = i
         newButton.direction = -1
-        downButtons.append(newButton)
+        stageButtons.append(newButton)
     i+=1
+def MoveIt():#deside where to go and where to stop with your life
+#cabin place
+    print('Cabin devastates the floor '+str(theCabin.stage))
+    print('Cabin is '+theCabin.status)
+    print('Cabin Thinks about going to add '+str(theCabin.direction)+' to its floor count')
+#state of buttons inside
+#only on the same side we move to or all
+    
+# buttons outside
+#same side of movement
+# else all
+    
+# desision is made 
+
+#buttons on curent flour changed to false
+
+  
+
+
+
 def Call(place,stage,direction=0):
     #plase is c or s
     #stage is stage lala lala la
@@ -46,12 +65,21 @@ def Call(place,stage,direction=0):
     if place == 'c':
         #CabinButtons[stage].status = True
         print('Cabin internall button summons thea to '+str(stage))
+        for oneButton in CabineButtons:#goes throught all the elements of array
+            if oneButton.stage ==stage:
+                oneButton.status=True
+
     if place == 's':
         if direction==1:
             dirText=" to Stars"
         if direction==-1:
             dirText=' to the Deps Of Hell'
         print('Floor button summons thea to '+str(stage)+dirText)
-    
-Call("c",4,1)
-Call("s",4,1)
+        for oneButton in stageButtons:
+            if oneButton.stage==stage and oneButton.direction==direction
+                oneButton.status=True
+    MoveIt()  
+
+  
+Call("c",34)
+Call("s",4,-1)
