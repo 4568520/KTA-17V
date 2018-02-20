8e1dc3051763c80700111dd5a5a6ea72140c59f6
Ilja Kazantsev <33126420+4568520@users.noreply.github.com>
2018-02-20 14:07:56 +0200

    1

---
+++ lift/lift.py
@@ -0,0 +1,8 @@
+
+
+class Cabine:
+    stage=0# Floor numeric value that some how his name
+    direction=0 #if -1 - goes down 1 - goes up 0 - stop and load
+    status = 'free'#'free' - searchinr for a couple 'ordered'- will have intedcorse with somthind going in to him 
+class Floor:
+    name=0
