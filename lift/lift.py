8285a47b389e122d8c3cb073b1326a94abf7bd90
Ilja Kazantsev <33126420+4568520@users.noreply.github.com>
2018-02-20 14:20:26 +0200

    2

--- lift/lift.py
+++ lift/lift.py
@@ -4,5 +4,14 @@ class Cabine:
     stage=0# Floor numeric value that some how his name
     direction=0 #if -1 - goes down 1 - goes up 0 - stop and load
     status = 'free'#'free' - searchinr for a couple 'ordered'- will have intedcorse with somthind going in to him 
-class Floor:
-    name=0
+class CadinButton:
+    status=False # summon the great devouring BOX OF CHROMIUM
+    stage=0#the floor on with you performed the Ainchient ritual to summon the great devouring BOX OF CHROMIUM
+class StageButton:
+    status=False # summon the great devouring BOX OF CHROMIDOOM
+    stage=0#the floor on with you performed the Ainchient ritual to summon the great devouring BOX OF CHROMIDOOM
+    direction=1 #if -1 - goes down 1 - goes up 0 is not for you button fella
+
+
+theCabin = Cabine()#Object CROMIUM DEATH IS CREATED
+print(theCabine.stage)
