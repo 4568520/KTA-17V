5386ea242a2384d33a326e01a205be89ae670304
Ilja Kazantsev <33126420+4568520@users.noreply.github.com>
2018-02-20 14:33:38 +0200

    in case

--- lift/lift.py
+++ lift/lift.py
@@ -4,7 +4,7 @@ class Cabine:
     stage=0# Floor numeric value that some how his name
     direction=0 #if -1 - goes down 1 - goes up 0 - stop and load
     status = 'free'#'free' - searchinr for a couple 'ordered'- will have intedcorse with somthind going in to him 
-class CadinButton:
+class CabineButton:
     status=False # summon the great devouring BOX OF CHROMIUM
     stage=0#the floor on with you performed the Ainchient ritual to summon the great devouring BOX OF CHROMIUM
 class StageButton:
@@ -14,4 +14,17 @@ class StageButton:
 
 
 theCabin = Cabine()#Object CROMIUM DEATH IS CREATED
-print(theCabine.stage)
+print(theCabin.status)
+
+N=78 #Floors OF DEATH HOROR AND CULTS OF OLD ONE'S
+CabinButtons = []
+StageButtons = []
+i=0
+while i<=N:
+    newButton=CabineButton()
+    newButton.stage = i
+    CabinButtons.append(newButton)
+    print(CabinButtons[i].stage)
+    i+=1
+
+
