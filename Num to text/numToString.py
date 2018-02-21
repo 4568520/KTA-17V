6bad94932379c30bf3bad3ed9b624678df8553a3
Ilja Kazantsev <33126420+4568520@users.noreply.github.com>
2018-02-21 11:34:17 +0200

    55

--- Num to text/numToString.py
+++ Num to text/numToString.py
@@ -3,4 +3,27 @@
 #Courier и Courier New
 #Anonymous Pro
 
-def numToText
+def numToText(number):
+    simpNumbers= ['zero','one','two','three','four','five','six','seven','eight','nine']
+    tenNumbers=['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+    decadesNum=['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+    logTen =['hundred','thousand','million',]
+    outPutString=''
+    numberStr= str(number)
+    #if not number.isnumeric():
+     #   outPutString='NaN'
+      #  return outPutString
+    for num in numberStr:
+
+
+    if number < 10:
+        outPutString = simpNumbers[number]
+        return outPutString
+    elif number < 20:
+        number-=10
+        outPutString = tenNumbers[number]
+        return outPutString
+    elif number > 19 and number < 100:
+        if numberStr[1]=='0':
+
+
--- Num to text/test.py
+++ Num to text/test.py
@@ -1,7 +1,6 @@
 
+import numToString
 
 
-
-g=input("need a number ")
-
-numToText()
+for x in range(0,20):
+    print(numToString.numToText(x))
